4a907f57c8a584f9bb62d1e42ea5edaca814b356
RamsesCamas <machiniram@gmail.com>
2022-03-14 21:28:52 -0600

    More entries

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def perceptron(X,W):
     u = X.dot(W)
     return step(u)
 
-def train(X,W, alpha, yd):
+def entrenar(X,W, taza_aprendizaje, yd):
     errors = []
     norm_err = 1
     Wk = W
@@ -17,7 +17,7 @@ def train(X,W, alpha, yd):
         yc = perceptron(X,Wk)
         ek = yd - yc
         norm_err = np.linalg.norm(ek)
-        Wk = Wk  + (alpha*(ek.T.dot(X)))
+        Wk = Wk  + (taza_aprendizaje*(ek.T.dot(X)))
         errors.append(norm_err)
     return errors , Wk
 
@@ -33,11 +33,12 @@ def normalize_arrays(total_errors, max_total_errors):
         Y_s.append(error_array)
     return Y_s
 
-def execute_nn():
+def ejecutar_perceptron():
     X_dos_entradas = np.array([[1,0,0],
                                [1,0,1],
                                [1,1,0],
                                [1,1,1]])
+
     W0_dos_entradas = np.random.rand(3)
     X_tres_entradas = np.array([[1,0,0,0],
                                 [1,0,0,1],
@@ -49,33 +50,40 @@ def execute_nn():
                                 [1,1,1,1]])
 
     W0_tres_entradas = np.random.rand(4)
-    label_FW['text'] = f'W0: {W0_dos_entradas}'
-    A1 = float(entry_alpha1.get())
-    A2 = float(entry_alpha2.get())
-    A3 = float(entry_alpha3.get())
-    A4 = float(entry_alpha4.get())
-    A5 = float(entry_alpha5.get())
-    alphas = [A1,A2,A3,A4,A5] 
+    label_FW['text'] = f'Pesos iniciales: {W0_dos_entradas}'
+    TA1 = float(entry_taza_aprendizaje1.get())
+    TA2 = float(entry_taza_aprendizaje2.get())
+    TA3 = float(entry_taza_aprendizaje3.get())
+    TA4 = float(entry_taza_aprendizaje4.get())
+    TA5 = float(entry_taza_aprendizaje5.get())
+    taza_aprendizajes = [TA1,TA2,TA3,TA4,TA5] 
 
     Y = str(entry_yd.get())
     Yd = np.array(list(map(int, Y.split(','))))
 
     total_errors, W_s = [] , []
-    for alpha in alphas:
-        error, F_W = train(X_tres_entradas,W0_tres_entradas,alpha,Yd) 
+    for taza_aprendizaje in taza_aprendizajes:
+        error, F_W = entrenar(X_dos_entradas,W0_dos_entradas,taza_aprendizaje,Yd) 
         total_errors.append(error)
         W_s.append(F_W)
     max_total_errors = len(max(total_errors,key=len))
     Y_s = normalize_arrays(total_errors,max_total_errors)
 
-    title = 'Evolución de la norma del error'
+    title = 'Norma del error'
 
     x = np.arange(0,len(Y_s[0]))
-    my_colors = ['r','g','b','c','m']
-    label_Ws = [label_W0,label_W1,label_W2,label_W3,label_W4]
-    for y, my_color, alpha,my_label, my_W in zip(Y_s,my_colors,alphas,label_Ws,W_s):
-        plt.plot(x,y,color=my_color,label=f'Alpha {alpha}')
-        my_label['text'] = f'W final con Alpha {alpha}: {my_W}'
+
+    plt.plot(x,Y_s[0],color='green',label=f'taza_aprendizaje {TA1}')
+    label_W0['text'] = f'Pesos finales con taza_aprendizaje {TA1}: {W_s[0]}'
+    plt.plot(x,Y_s[1],color='red',label=f'taza_aprendizaje {TA2}')
+    label_W1['text'] = f'Pesos finales con taza_aprendizaje {TA1}: {W_s[1]}'
+    plt.plot(x,Y_s[2],color='blue',label=f'taza_aprendizaje {TA3}')
+    label_W2['text'] = f'Pesos finales con taza_aprendizaje {TA1}: {W_s[2]}'
+    plt.plot(x,Y_s[3],color='orange',label=f'taza_aprendizaje {TA4}')
+    label_W3['text'] = f'Pesos finales con taza_aprendizaje {TA1}: {W_s[3]}'
+    plt.plot(x,Y_s[4],color='cyan',label=f'taza_aprendizaje {TA5}')
+    label_W4['text'] = f'Pesos finales con taza_aprendizaje {TA1}: {W_s[4]}'
+    
     plt.legend(loc="upper right")
     plt.xlabel('Iteraciones')
     plt.ylabel('Error')
@@ -88,27 +96,27 @@ if __name__=='__main__':
     root.geometry('800x400')
     root.title('Perceptrón')
 
-    label_alpha1 = Label(root, width=15, font=my_font,text='Alpha 1')
-    label_alpha1.place(x=30,y=20)
-    label_alpha2 = Label(root, width=15, font=my_font,text='Alpha 2')
-    label_alpha2.place(x=200,y=20)
-    label_alpha3 = Label(root, width=15, font=my_font,text='Alpha 3')
-    label_alpha3.place(x=350,y=20)
-    label_alpha4 = Label(root, width=15, font=my_font,text='Alpha 4')
-    label_alpha4.place(x=500,y=20)
-    label_alpha5 = Label(root, width=15, font=my_font,text='Alpha 5')
-    label_alpha5.place(x=650,y=20)
+    label_taza_aprendizaje1 = Label(root, width=15, font=my_font,text='taza_aprendizaje 1')
+    label_taza_aprendizaje1.place(x=30,y=20)
+    label_taza_aprendizaje2 = Label(root, width=15, font=my_font,text='taza_aprendizaje 2')
+    label_taza_aprendizaje2.place(x=200,y=20)
+    label_taza_aprendizaje3 = Label(root, width=15, font=my_font,text='taza_aprendizaje 3')
+    label_taza_aprendizaje3.place(x=350,y=20)
+    label_taza_aprendizaje4 = Label(root, width=15, font=my_font,text='taza_aprendizaje 4')
+    label_taza_aprendizaje4.place(x=500,y=20)
+    label_taza_aprendizaje5 = Label(root, width=15, font=my_font,text='taza_aprendizaje 5')
+    label_taza_aprendizaje5.place(x=650,y=20)
 
-    entry_alpha1 = Entry(root,width=14, font=my_font)
-    entry_alpha1.place(x=30,y=50)
-    entry_alpha2 = Entry(root,width=14, font=my_font)
-    entry_alpha2.place(x=200,y=50)
-    entry_alpha3 = Entry(root,width=14, font=my_font)
-    entry_alpha3.place(x=350,y=50)
-    entry_alpha4 = Entry(root,width=14, font=my_font)
-    entry_alpha4.place(x=500,y=50)
-    entry_alpha5 = Entry(root,width=14, font=my_font)
-    entry_alpha5.place(x=650,y=50)
+    entry_taza_aprendizaje1 = Entry(root,width=14, font=my_font)
+    entry_taza_aprendizaje1.place(x=30,y=50)
+    entry_taza_aprendizaje2 = Entry(root,width=14, font=my_font)
+    entry_taza_aprendizaje2.place(x=200,y=50)
+    entry_taza_aprendizaje3 = Entry(root,width=14, font=my_font)
+    entry_taza_aprendizaje3.place(x=350,y=50)
+    entry_taza_aprendizaje4 = Entry(root,width=14, font=my_font)
+    entry_taza_aprendizaje4.place(x=500,y=50)
+    entry_taza_aprendizaje5 = Entry(root,width=14, font=my_font)
+    entry_taza_aprendizaje5.place(x=650,y=50)
 
     label_Ys = Label(root, width=15, font=my_font, text='Y determinada:')
     label_Ys.place(x=20,y=150)
@@ -118,7 +126,7 @@ if __name__=='__main__':
 
     label_yd = Label(root,width=30,font=my_font,text='Y determinada').place(x=350,y=170)
 
-    btn_exec_nn = Button(root, width=14, text='Ejecutar', font=my_font, command=execute_nn)
+    btn_exec_nn = Button(root, width=14, text='Ejecutar', font=my_font, command=ejecutar_perceptron)
     btn_exec_nn.place(x=600,y=145)
 
     label_FW = Label(root, width=70, font=my_font, text='')
@@ -134,9 +142,5 @@ if __name__=='__main__':
     label_W4 = Label(root, width=70, font=my_font, text='')
     label_W4.place(x=0,y=340)
 
-    label_and = Label(root, width=15, font=my_font, text='Y AND: 0 0 0 1')
-    label_and.place(x=600,y=220)
-    label_or = Label(root, width=15, font=my_font, text='Y OR: 0 1 1 1')
-    label_or.place(x=600,y=250)
 
     root.mainloop()
